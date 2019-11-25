e49203e345581606cd6add6248b388ba4b8ba476
Philip Edwards <philip@gdcorner.com>
2019-11-25 12:45:46 +1100

    Refactored to not use globals.

--- UpscaleTextures.py
+++ UpscaleTextures.py
@@ -1,20 +1,33 @@
 import json
+import pprint
 
 from TextureUpscaler.TextureProcessing import *
 from TextureUpscaler.UpscaleESRGAN import *
 
-settingsFile = open("settings.json")
-settings = json.load(settingsFile)
-settingsFile.close()
+def load_settings():
+    settingsFile = open("settings.json")
+    settings = json.load(settingsFile)
+    settingsFile.close()
+    return settings
 
-SourcePath = settings["SourcePath"]
-WorkingPath = settings["WorkingPath"]
-ExtensionsToFind = settings["ExtensionsToFind"]
-ESRGANModel = settings["ESRGANModel"]
+def print_settings(settings):
+    pprint.pprint(settings)
 
-if __name__ == "__main__":
+def run_texture_processing_pipeline():
+    settings = load_settings()
+    print_settings(settings)
+
+    SourcePath = settings["SourcePath"]
+    WorkingPath = settings["WorkingPath"]
+    ExtensionsToFind = settings["ExtensionsToFind"]
+    ESRGANModel = settings["ESRGANModel"]
+
     images = gather_textures(SourcePath, WorkingPath, ExtensionsToFind)
-    print(len(images))
+    print("Number of images gathered: " + str(len(images)))
+
     run_processing_stage(denoise_texture, images)
     upscale_esrgan(images, WorkingPath, ESRGANModel)
     run_processing_stage(save_hires_image, images)
+
+if __name__ == "__main__":
+    run_texture_processing_pipeline()
